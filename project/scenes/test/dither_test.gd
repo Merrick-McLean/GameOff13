97d4b4607e4bf0471c82fe05b419c6e3e6f9538a
RNSF <ahrenrsf@gmail.com>
2024-11-08 15:27:02 -0800

    cross hatching test

---
+++ project/scenes/test/dither_test.gd
@@ -0,0 +1,9 @@
+@tool
+extends SubViewportContainer
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#material.set_shader_parameter("u_dither_offset", Vector2(Time.get_ticks_msec() / 1000.0 * 0.04,  Time.get_ticks_msec() / 1000.0 * 0.04))
+	pass
