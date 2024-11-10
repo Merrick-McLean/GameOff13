b08721d31ec79f169559bab690bf38f28e06c273
Merrick McLean <merrickmmclean@gmail.com>
2024-11-09 16:34:02 -0800

    Add unorganised skeleton of liars dice logic/functions

--- project/scenes/dice_roll.gd
+++ project/scenes/dice_roll.gd
@@ -1,45 +1,51 @@
+### SETUP ###
 @tool
 extends Node3D
 
+class_name dice_roll
+
+# set die model
 var die = preload("res://models/die.glb")
+# set cup model
+# var cup = preload("res://models/cup.glb")
 
-var face: Dictionary = {1: Vector3(90,0,0), 2: Vector3(180,0,0), 3: Vector3(0,0,90), 4: Vector3(0,0,270), 5: Vector3(0,0,0), 6: Vector3(270,0,0)}
+# we also want to randomize the y value to give a sense opf randomness
+var face: Dictionary = {1: Vector3(90,randi_range(0,360),0), 2: Vector3(180,randi_range(0,360),0), 3: Vector3(0,randi_range(0,360),90), 4: Vector3(0,randi_range(0,360),270), 5: Vector3(0,randi_range(0,360),0), 6: Vector3(270,randi_range(0,360),0)}
+const anchors: Dictionary = {0: Vector3(0,0,0), 1: Vector3(0,0,0), 2: Vector3(0,0,0), 3: Vector3(0,0,0)}
+const dice_per = 5
 
+### CAPABILTIES ###
+# Function to create 5 non-overlapping dice based upon the given values and the anchor location of cup for the NPC
+# TODO: once we have environment and cup, add dice distribution
+# TODO: linkt to decision making algorithm which will provide the dice values
+func _create_dice(values: Array, target: int) -> void:
+	for i in dice_per:
+		_spawn_die(values[i], anchors[target]) # use the i to vary the locations of the dice (or physics)
+	pass
+
+func _create_player_dice() -> Array:
+	var rng = RandomNumberGenerator.new()
+	var values = Array()
+	for i in dice_per:
+		var value = rng.randi_range(1,6)
+		values[i] = value
+	_create_dice(values, 0)
+	return values
+
+# Function to spawn a single die at a given location in 3d space with a certain value
+func _spawn_die(value: int, loc: Vector3) -> void:
+	print("creating die")
+	var dieInstance = die.instantiate()
+	dieInstance.transform.origin = loc
+	dieInstance.rotation_degrees = face[value]
+	add_child(dieInstance)
+	pass
+
+### DEFAULT ###
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var rng = RandomNumberGenerator.new()
-	var value = rng.randi_range(1,6)
-	_spawn_die(value)
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _spawn_die(value):
-	print("creating die")
-	var dieInstance = die.instantiate()
-	dieInstance.transform.origin = Vector3(0,0,0)
-	
-	match value:
-		1:
-			print("1")
-			dieInstance.rotation_degrees = face[1]
-		2: 
-			print("2")
-			dieInstance.rotation_degrees = face[2]
-		3: 
-			print("3")
-			dieInstance.rotation_degrees = face[3]
-		4: 
-			print("4")
-			dieInstance.rotation_degrees = face[4]
-		5: 
-			print("5")
-			dieInstance.rotation_degrees = face[5]
-		6: 
-			print("6")
-			dieInstance.rotation_degrees = face[6]
-
-	add_child(dieInstance)
