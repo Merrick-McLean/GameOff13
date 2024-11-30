805fc4ab5812a551e037744ff51263df4af5a862
RNSF <ahrenrsf@gmail.com>
2024-11-30 00:43:24 -0800

    quit game after credits

--- project/scenes/ui/credits.gd
+++ project/scenes/ui/credits.gd
@@ -9,3 +9,9 @@ func play() -> void:
 
 func _on_liars_dice_physical_ready_for_credits() -> void:
 	play()
+
+
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	get_tree().quit()
